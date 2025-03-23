3a452a7d40c4c7af477a379441ab0377903453ce
Ali <alibenkarrouch@gmail.com>
2025-03-22 21:25:50 -0500

    Ajout de la bar de naviguation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import { Authentification } from './pages/Authentification';
-import { Home } from './pages/Home';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import { Authentification } from "./pages/Authentification";
+import { Home } from "./pages/Home";
+import { Explore } from "./pages/Explore";
+import rosemontLogo from "./assets/logos/RosemontLogoBrute.png";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={rosemontLogo} alt="logo" />
+        <Link to="/">Rosemont DevHub</Link>
+      </div>
+
+      <ul className="nav-links">
+      <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/explore">Explore</Link>
+        </li>
+        <div className="search-holder">
+          <i className="bi bi-search"></i>
+          <input type="text" placeholder="Rechercher..." />
+        </div>
+
+        <li>
+          <Link className="userLink" to="/authentification">
+            <i className="bi bi-person-fill"></i>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/authentification" element={<Authentification />} />
         <Route path="/" element={<Home />} />
+        <Route path="/" element={<Explore />} />
+        <Route path="/authentification" element={<Authentification />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Explore.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/style-explore.css";
+
+export const Explore = () => {
+    return(
+        <div>
+            <h1>Explore</h1>
+        </div>
+    );
+};
