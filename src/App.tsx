c83643ebde32993e061b60cd09c93062bc6a757d
Ali <alibenkarrouch@gmail.com>
2025-03-23 22:41:16 -0500

    Ajout d'un dialogue de profil pour les options de l'utilisateur connecté

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 import { Authentification } from "./pages/Authentification";
@@ -9,10 +9,24 @@ import { Profil } from "./pages/Profil";
 import { Watch } from "./pages/Watch";
 import { Publish } from "./pages/Publish";
 import { useAuth } from "./hooks/useAuth";
+import gsap from "gsap";
 
 function Navbar() {
 
   const { isLoggedIn, user, logout } = useAuth();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const dialogRef = useRef<HTMLDivElement>(null);
+
+  const toggleDialog = () => {
+    setIsDialogOpen((prev: unknown) => {
+      if (!prev) {
+        gsap.to(dialogRef.current, { opacity: 1, y: 0, duration: 0.3, display: "flex" });
+      } else {
+        gsap.to(dialogRef.current, { opacity: 0, y: -20, duration: 0.3, display: "none" });
+      }
+      return !prev;
+    });
+  };
 
   return (
     <nav className="navbar">
@@ -28,9 +42,6 @@ function Navbar() {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/profil/2">Profil</Link>
-        </li>
-        <li>
           <Link to="/explore">Explore</Link>
         </li>
         <div className="search-holder">
@@ -41,18 +52,21 @@ function Navbar() {
         {isLoggedIn ? (
           <>
             <li>
-              <button className="logout-button" onClick={logout}>
-                Déconnexion
-              </button>
-            </li>
-            <li>
-              <Link className="userLink" to={`/profil/${user?.id}`}>
+              <div className="userLink loggedUser" onClick={toggleDialog}>
                 <img
                   src={user?.avatar || "https://robohash.org/default.png"}
                   alt="Avatar"
                   className="user-avatar"
                 />
-              </Link>
+                <div className="user-dialog" ref={dialogRef} style={{ display: "none", opacity: 0 }}>
+                  <p>{user?.firstName} {user?.lastName}</p>
+                  <div className="user-dialog-links">
+                    <Link to={`/profil/${user?.id}`}>Profil</Link>
+                    <Link to="/publish">Publier</Link>
+                    <Link to="/authentification" onClick={logout}>Déconnexion</Link>
+                  </div>
+                </div>
+              </div>
             </li>
           </>
         ) : (
