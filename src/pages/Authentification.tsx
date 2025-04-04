d927812a65d871a663479d6232ce30a409c45e1c
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2025-04-04 17:20:20 -0400

    Fix animation login

--- src/pages/Authentification.tsx
+++ src/pages/Authentification.tsx
@@ -17,8 +17,10 @@ export const Authentification = () => {
     if (isLogin) {
       tl.to(loginRef.current, {
         opacity: 0,
+        scale: 0.8,
         filter: "blur(10px)",
-        duration: 0.25,
+        duration: 0.3,
+        ease: "power1.in",
         onComplete: () => {
           gsap.set(loginRef.current, { display: "none" });
           setIsLogin(false);
@@ -28,46 +30,85 @@ export const Authentification = () => {
 
       tl.fromTo(
         signupRef.current,
-        { opacity: 0, filter: "blur(10px)" },
-        { opacity: 1, filter: "blur(0px)", duration: 0.25 }
-      );
+        { opacity: 0, scale: 0.8, filter: "blur(10px)" },
+        {
+          opacity: 1,
+          scale: 1.1,
+          filter: "blur(0px)",
+          duration: 0.4,
+          ease: "back.out(1.7)",
+        }
+      ).to(signupRef.current, {
+        scale: 1,
+        duration: 0.2,
+        ease: "power1.out",
+      });
     } else {
       tl.to(signupRef.current, {
         opacity: 0,
+        scale: 0.8,
         filter: "blur(10px)",
-        duration: 0.25,
+        duration: 0.3,
+        ease: "power1.in",
         onComplete: () => {
           gsap.set(signupRef.current, { display: "none" });
-          setIsLogin(true)
+          setIsLogin(true);
           gsap.set(loginRef.current, { display: "block" });
         },
       });
 
       tl.fromTo(
         loginRef.current,
-        { opacity: 0, filter: "blur(30px)" },
-        { opacity: 1, filter: "blur(0px)", duration: 0.25 }
-      );
+        { opacity: 0, scale: 0.8, filter: "blur(10px)" },
+        {
+          opacity: 1,
+          scale: 1.1,
+          filter: "blur(0px)",
+          duration: 0.4,
+          ease: "back.out(1.7)",
+        }
+      ).to(loginRef.current, {
+        scale: 1,
+        duration: 0.2,
+        ease: "power1.out",
+      });
     }
   };
 
   useEffect(() => {
     if (isLogin) {
-      gsap.set(loginRef.current, { opacity: 1, filter: "blur(0px)", display: "block" });
+      gsap.set(loginRef.current, {
+        opacity: 1,
+        filter: "blur(0px)",
+        display: "block",
+      });
       gsap.set(signupRef.current, { opacity: 0, display: "none" });
     } else {
-      gsap.set(signupRef.current, { opacity: 1, filter: "blur(0px)", display: "block" });
+      gsap.set(signupRef.current, {
+        opacity: 1,
+        filter: "blur(0px)",
+        display: "block",
+      });
       gsap.set(loginRef.current, { opacity: 0, display: "none" });
     }
   }, [isLogin]);
 
   return (
     <div className="authentification-container">
-
-      <div ref={loginRef} className="form-wrapper">
+      <div
+        ref={loginRef}
+        className="form-wrapper"
+        style={{
+          display: isLogin ? "block" : "none",
+        }}
+      >
         <LoginForm toggleForm={toggleForm} />
       </div>
-      <div ref={signupRef} className="form-wrapper">
+      <div
+        ref={signupRef}
+        className="form-wrapper"
+        style={{ display: !isLogin ? "block" : "none" }}
+      >
         <Signup toggleForm={toggleForm} />
       </div>
       <BackgroundCarousel />
