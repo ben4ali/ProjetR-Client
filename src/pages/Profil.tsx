e980bbcce06944bde6e0ddca428a7a36c5d76deb
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2025-06-11 22:02:37 -0400

    fix change user pics dialog

--- src/pages/Profil.tsx
+++ src/pages/Profil.tsx
@@ -3,14 +3,17 @@ import { ProfilBody } from "../components/profil/ProfilBody";
 import { useUserById } from "../hooks/use-users";
 import { useProjectsByUserId } from "../hooks/use-project";
 import { useCurrentUser } from "../hooks/use-auth";
+import { useMemo } from "react";
 
 export const Profil = () => {
   const id = window.location.pathname.split("/").pop();
   const { data: user } = useUserById(id);
   const { data: projets } = useProjectsByUserId(id);
   const { data: currentUser } = useCurrentUser();
-
-  const isCurrentUser = currentUser?.id === id;
+  const isCurrentUser = useMemo(
+    () => currentUser?.id === user?.id,
+    [currentUser, user]
+  );
 
   return (
     <div className="flex flex-col items-center pb-12">
