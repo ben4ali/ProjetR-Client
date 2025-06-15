f2f81a94e6ae970bfa2806a24683081a78f7decd
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2025-06-14 23:17:33 -0400

    autoplay sur watch

--- src/components/watch/VideoPlayer.tsx
+++ src/components/watch/VideoPlayer.tsx
@@ -13,11 +13,12 @@ export const VideoPlayer = ({ projet }: VideoPlayerProps) => {
   }, [projet?.demoUrl]);
 
   return (
-    <div className="video-container w-full h-[15rem] lg:h-[35rem]">
+    <div className="video-container h-[15rem] w-full lg:h-[35rem]">
       <video
         ref={videoRef}
         controls
-        className="video w-full h-[15rem] lg:h-[35rem] object-contain rounded bg-black"
+        className="video h-[15rem] w-full rounded bg-black object-contain lg:h-[35rem]"
+        autoPlay
       >
         <source src={projet?.demoUrl} type="video/mp4" />
         Votre navigateur ne prend pas en charge la lecture de vidéos.
